1adb8a1c27743125e63ed2aae40e506a1e666fb1
SuelenGalhardo <suelenranucci@gmail.com>
2024-05-14 15:36:58 +0200

    added  user registration features

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,43 @@
+import { useState } from "react";
 import "../styles/App.scss";
 import "../styles/layout/SignUp.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { api } from "../services/api";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 import { FiMail, FiLock, FiUser } from "react-icons/fi";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp(){
+    //console.log(name, email, password);
+    // (name === "" || email === "" || password === "") return para function parar aqui 
+    if(!name ||!email ||!password ){
+     return alert("Todos los campos son obligatorios");
+  }
+  api.post("/users", {name, email, password})
+  .then(()=> {
+    alert("Usuario creado con exito");
+    navigate("/");
+   })
+
+    .catch(error => {
+      if(error.response){
+        alert(error.response.data.message);
+
+      } else { 
+      alert("Ocurrio un error al crear el usuario");
+    }
+  });
+}
+
+
+
   return (
     <>
     <div className="SignUp">
@@ -19,11 +51,11 @@ export function SignUp() {
         <h2 className="SignUp__titleTwo"> Crear su cuenta  </h2>
 
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="password" type="text" icon={FiLock} />
+        <Input placeholder="Nome" type="text" icon={FiUser} onChange={e => setName(e.target.value)}/>
+        <Input placeholder="E-mail" type="text" icon={FiMail} onChange={e => setEmail(e.target.value)} />
+        <Input placeholder="password" type="text" icon={FiLock} onChange={e => setPassword(e.target.value)}/>
 
-        <Button title="Registrar" />
+        <Button title="Registrar" onClick={handleSignUp}/>
         <Link to="/"  className="SignUp__createCont">
         Volver a iniciar sesión
         </Link>
