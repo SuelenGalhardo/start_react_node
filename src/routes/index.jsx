e35de6feefd70b0e804d2b6847d5f3a1e6c34d43
SuelenGalhardo <suelenranucci@gmail.com>
2024-05-15 15:37:17 +0200

    added redirecting routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
-
+import { useAuth } from "../hooks/auth";
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+  const { user } = useAuth();
   return (
+
+    //if ternario user es true, renderiza AppRoutes, sino renderiza AuthRoutes
+
     <BrowserRouter>
-      <AuthRoutes />
+      { user ? <AppRoutes /> : <AuthRoutes />}
     </BrowserRouter>
   )
 }
